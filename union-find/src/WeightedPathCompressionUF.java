4d305b054fe5f0c3f0932d6addd90df4db627d7d
Tobibur Rahman <tobiburrahman786@gmail.com>
2020-09-10 22:14:09 +0530

    sz counter fix

--- union-find/src/WeightedPathCompressionUF.java
+++ union-find/src/WeightedPathCompressionUF.java
@@ -8,6 +8,7 @@ public class WeightedPathCompressionUF {
         sz = new int[N];
         for (int i = 0; i < N; i++) {
             id[i] = i;
+            sz[i] = 1;
         }
     }
 
--- union-find/src/WeightedQuickUnionUF.java
+++ union-find/src/WeightedQuickUnionUF.java
@@ -6,8 +6,10 @@ public class WeightedQuickUnionUF {
     public WeightedQuickUnionUF(int N){
         id = new int[N];
         sz = new int[N];
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++) {
             id[i] = i;
+            sz[i] = 1;
+        }
     }
 
     private int root(int i){
